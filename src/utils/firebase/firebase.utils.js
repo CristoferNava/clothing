1b41e1c1dc92a4105e7d28e54755e00c8b86185e
CristoferNava <cristofernava11719@gmail.com>
2023-07-12 20:54:44 -0600

    Signin with pop was added.

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,58 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBehOHKlQaKSN-r1tnkscw2mXk821qtfqs",
+  authDomain: "duck-gb-db.firebaseapp.com",
+  projectId: "duck-gb-db",
+  storageBucket: "duck-gb-db.appspot.com",
+  messagingSenderId: "676193899859",
+  appId: "1:676193899859:web:bb8acdbebc5f7c9766bda6",
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+  const userSnapshot = await getDoc(userDocRef);
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log("Error creating the user: ", error.message);
+    }
+  }
+
+  return userDocRef;
+};
