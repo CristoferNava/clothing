ef71d7e92489d58402f0f1539a357b09c0db1f6f
CristoferNava <cristofernava11719@gmail.com>
2023-07-24 15:45:44 -0600

    Links to the categories were added to the home page.

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,8 +1,14 @@
+import { useNavigate } from "react-router-dom";
+
 import "./directory-item.styles.scss";
 
-const DirectoryItem = ({ title, imageUrl }) => {
+const DirectoryItem = ({ category }) => {
+  const { title, imageUrl, route } = category;
+
+  const navigate = useNavigate();
+
   return (
-    <div className="directory-item-container">
+    <div className="directory-item-container" onClick={() => navigate(route)}>
       <div
         className="background-image"
         style={{ backgroundImage: `url(${imageUrl})` }}
